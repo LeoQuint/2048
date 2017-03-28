c85b85e87ae974d4b3de909c1930ad3f9921308a
Graphic <graphic@SOD-SJH-611-01M.local>
2017-03-28 11:40:01 -0400

    colors and shits

--- 2048/2048/GameScene.swift
+++ 2048/2048/GameScene.swift
@@ -14,8 +14,32 @@ class GameScene: SKScene {
     private var btn_newGame: SKLabelNode?
     private var btn_return: SKLabelNode?
     private var selectedShape: SKShapeNode?
-    private var selecedLabel: SKLabelNode?
+    private var selectedLabel: SKLabelNode?
     
+    
+    
+    //Colors tiles
+    private var color_Zero: SKColor? = SKColor (colorLiteralRed: 0.1, green: 0.2, blue: 1.0, alpha: 1.0)
+    private var color_P1: SKColor? = SKColor (colorLiteralRed: 0.1, green: 0.2, blue: 1.0, alpha: 1.0)//2
+    private var color_P2: SKColor? = SKColor (colorLiteralRed: 0.1, green: 0.2, blue: 1.0, alpha: 1.0)//4
+    private var color_P3: SKColor? = SKColor (colorLiteralRed: 0.1, green: 0.2, blue: 1.0, alpha: 1.0)//8
+    private var color_P4: SKColor? = SKColor (colorLiteralRed: 0.1, green: 0.2, blue: 1.0, alpha: 1.0)//16
+    private var color_P5: SKColor? = SKColor (colorLiteralRed: 0.1, green: 0.2, blue: 1.0, alpha: 1.0)//32
+    private var color_P6: SKColor? = SKColor (colorLiteralRed: 0.1, green: 0.2, blue: 1.0, alpha: 1.0)//64
+    private var color_P7: SKColor? = SKColor (colorLiteralRed: 0.1, green: 0.2, blue: 1.0, alpha: 1.0)//128
+    private var color_P8: SKColor? = SKColor (colorLiteralRed: 0.1, green: 0.2, blue: 1.0, alpha: 1.0)//256
+    private var color_P9: SKColor? = SKColor (colorLiteralRed: 0.1, green: 0.2, blue: 1.0, alpha: 1.0)//516
+    private var color_P10: SKColor? = SKColor (colorLiteralRed: 0.1, green: 0.2, blue: 1.0, alpha: 1.0)//1024
+    private var color_P11: SKColor? = SKColor (colorLiteralRed: 0.1, green: 0.2, blue: 1.0, alpha: 1.0)//2048
+    //colors font
+    private var color_Dark: SKColor? = SKColor (colorLiteralRed: 0.2, green: 0.2, blue: 0.2, alpha: 1.0)
+    private var color_Light: SKColor? = SKColor (colorLiteralRed: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    //Font size
+    private var font_1D = 48
+    private var font_2D = 46
+    private var font_3D = 40
+    private var font_4D = 36
+
     
     var board = Array(repeating: Array(repeating: 0, count: 4), count: 4)
     
@@ -49,10 +73,7 @@ class GameScene: SKScene {
        
     }
     
-    func ChangeNodeNumber(row:Int, col:Int, value:Int){
-        self.selectedShape = self.childNode(withName: (String(row) + String(col))) as? SKShapeNode
-        self.selecedLabel = selectedShape?.childNode(withName: "num") as? SKLabelNode
-    }
+
     
     
     
@@ -66,6 +87,7 @@ class GameScene: SKScene {
     
     func swipedUp(recognizer:UISwipeGestureRecognizer){
         print("swiped up")
+        ChangeNodeNumber(row: 2,col: 2,value: 2)
     }
     
     func swipedDown(recognizer:UISwipeGestureRecognizer){
@@ -96,21 +118,53 @@ class GameScene: SKScene {
         
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    
+    func ChangeNodeNumber(row:Int, col:Int, value:Int){
+        self.selectedShape = self.childNode(withName: (String(row) + String(col))) as? SKShapeNode
+        self.selectedLabel = selectedShape?.childNode(withName: "num") as? SKLabelNode
         
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        switch value {
+        case 0:
+            selectedShape?.fillColor = color_Zero!
+        case 2:
+            selectedShape?.fillColor = color_P1!
+        case 4:
+            selectedShape?.fillColor = color_P2!
+        case 8:
+            selectedShape?.fillColor = color_P3!
+        case 16:
+            selectedShape?.fillColor = color_P4!
+        case 32:
+            selectedShape?.fillColor = color_P5!
+        case 64:
+            selectedShape?.fillColor = color_P6!
+        case 128:
+            selectedShape?.fillColor = color_P7!
+        case 256:
+            selectedShape?.fillColor = color_P1!
+        case 516:
+            selectedShape?.fillColor = color_P1!
+        case 1024:
+            selectedShape?.fillColor = color_P1!
+        case 2048:
+            selectedShape?.fillColor = color_P1!
+        default:
+            selectedShape?.fillColor = color_Zero!
+        }
         
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        
+        if (value < 5)
+        {
+            selectedLabel?.fontColor = color_Dark
+        }
+        else
+        {
+            selectedLabel?.fontColor = color_Dark
+        }
+        selectedLabel?.text = String(value)
+        
+        
     }
     
     func InitializeGame(){
